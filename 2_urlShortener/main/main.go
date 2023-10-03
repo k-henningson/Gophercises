458a1f2ec914060a016587770716dbc3c50ee995
Kyra Henningson <kyrahenningson@hotmail.com>
2023-10-02 19:40:26 -0700

    fix yaml indentation && import statement

--- 2_urlShortener/main/main.go
+++ 2_urlShortener/main/main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"fmt"
 	"net/http"
+
+	"./urlshort"
 )
 
 func main() {
@@ -18,11 +20,11 @@ func main() {
 	// Build the YAMLHandler using the mapHandler as the
 	// fallback
 	yaml := `
-	- path: /urlshort
-	  url: https://github.com/gophercises/urlshort
-	- path: /urlshort-final
-	  url: https://github.com/gophercises/urlshort/tree/solution
-	`
+- path: /urlshort
+  url: https://github.com/gophercises/urlshort
+- path: /urlshort-final
+  url: https://github.com/gophercises/urlshort/tree/solution
+`
 	yamlHandler, err := urlshort.YAMLHandler([]byte(yaml), mapHandler)
 	if err != nil {
 		panic(err)

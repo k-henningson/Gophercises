07c6869b5579a30c84dee0330f6c6c37f3c7654e
Kyra Henningson <kyrahenningson@hotmail.com>
2023-10-26 16:07:27 -0700

    add shapes and grid to pdf

--- 20_buildingPdfs/main.go
+++ 20_buildingPdfs/main.go
@@ -26,8 +26,39 @@ func main() {
 	_, lineHt = pdf.GetFontSize()
 	pdf.MultiCell(0, lineHt*1.5, "This is some text. If it is too long it will be word wrapped. If there is a new line it will\n be wrapped as well.", gofpdf.BorderNone, gofpdf.AlignRight, false)
 
+	// Shapes
+	pdf.SetFillColor(0, 255, 0)
+	pdf.SetDrawColor(0, 0, 255)
+	pdf.Rect(10, 100, 100, 100, "FD")
+	pdf.SetFillColor(100, 200, 200)
+	pdf.Polygon([]gofpdf.PointType{
+		{110, 250},
+		{160, 300},
+		{110, 350},
+		{60, 300},
+	}, "F")
+
+	// Grid
+	drawGrid(pdf)
+
 	err := pdf.OutputFileAndClose("p1.pdf")
 	if err != nil {
 		panic(err)
+	}
+}
+
+func drawGrid(pdf *gofpdf.Fpdf) {
+	w, h := pdf.GetPageSize()
+	for x := 0.0; x < w; x = x + (w / 20.0) {
+		pdf.SetFont("courier", "", 12)
+		pdf.SetTextColor(80, 80, 80)
+		pdf.SetDrawColor(200, 200, 200)
+		pdf.Line(x, 0, x, h)
+		_, lineHt := pdf.GetFontSize()
+		pdf.Text(x, lineHt, fmt.Sprintf("%d", int(x)))
+	}
+	for y := 0.0; y < h; y = y + (h / 20.0) {
+		pdf.Line(0, y, w, y)
+		pdf.Text(0, y, fmt.Sprintf("%d", int(y)))
 	}
 }
